5d769d41ff0f2d5f17c0cb2f2a052a94825eaf0b
brendan <brendan@brendansamek.com>
2021-03-29 21:00:47 -0600

    scaffolding for welcome page and navbar

--- src/App.js
+++ src/App.js
@@ -1,73 +1,86 @@
-import React, { useEffect } from "react";
-import { Container, Row, Col } from "reactstrap";
+import React, { useState, useEffect } from "react";
 
 import Map from "./features/map/Map";
 import LocationList from "./features/locations/LocationList";
 import "./App.css";
-import UserNav from "./features/locations/UserNav";
+import UserNav from "./features/Meta/NavBar";
 import LocationModal, {
   LocationOpener,
 } from "./features/locations/LocationModal";
 import { Route } from "react-router-dom";
-import MobileNav from "./features/locations/MobileNav";
 import Spinner from "./features/locations/Spinner";
 import { connect } from "react-redux";
+import { fetchMetaData } from "./features/Meta/metaSlice";
+import WelcomeSidebar from "./features/Meta/WelcomeSidebar";
 
 function App(props) {
   // Set Title properly
+  let { fetchMetaData } = props;
+
   useEffect(() => {
     if (props.currentPath === "") document.title = props.siteName;
   });
 
+  useEffect(() => {
+    fetchMetaData();
+  }, [fetchMetaData]);
+
   return (
     <>
-      <Spinner />
-      <Container fluid style={{ margin: 0, padding: 0, height: "100%" }}>
-        <Row
-          no-gutters="true"
-          style={{ margin: 0, padding: 0, height: "100%" }}
-        >
-          <Col
-            className="locations-sidebar"
-            lg="3"
-            style={{
-              height: "100%",
-              position: "absolute",
-              margin: 0,
-              padding: 0,
-              left: 0,
-              top: 0,
-            }}
-          >
-            <UserNav />
-            <LocationList />
-          </Col>
-          <Col
-            className="locations-sidebar"
-            lg="3"
-            style={{ height: "100%" }}
-          />
-
-          <Col
-            id="map"
-            lg="9"
-            style={{
-              margin: 0,
-              padding: 0,
-              height: "100vh",
-              position: "relative",
-            }}
-          >
-            <MobileNav className={"mobile-navbar"} />
-            {props.loading ? null : <Map />}
-          </Col>
-        </Row>
-      </Container>
-      <LocationModal />
-      <Route path="/:locationName" render={() => <LocationOpener />} />
+      <nav role="navigation" className="navbar">
+        <UserNav />
+      </nav>
+      <div className="sidebar">
+        <WelcomeSidebar />
+      </div>
+      <div className="map">{false ? null : <Map />}</div>
+      {/*<Spinner />*/}
+      {/*<Container fluid style={{ margin: 0, padding: 0, height: "100%" }}>*/}
+      {/*  <Row*/}
+      {/*    no-gutters="true"*/}
+      {/*    style={{ margin: 0, padding: 0, height: "100%" }}*/}
+      {/*  >*/}
+      {/*    <Col*/}
+      {/*      className="locations-sidebar"*/}
+      {/*      lg="3"*/}
+      {/*      style={{*/}
+      {/*        height: "100%",*/}
+      {/*        position: "absolute",*/}
+      {/*        margin: 0,*/}
+      {/*        padding: 0,*/}
+      {/*        left: 0,*/}
+      {/*        top: 0,*/}
+      {/*      }}*/}
+      {/*    >*/}
+      {/*      <UserNav />*/}
+      {/*      <LocationList />*/}
+      {/*    </Col>*/}
+      {/*    <Col*/}
+      {/*      className="locations-sidebar"*/}
+      {/*      lg="3"*/}
+      {/*      style={{ height: "100%" }}*/}
+      {/*    />*/}
+      {/*    <Col*/}
+      {/*      id="map"*/}
+      {/*      lg="9"*/}
+      {/*      style={{*/}
+      {/*        margin: 0,*/}
+      {/*        padding: 0,*/}
+      {/*        height: "100vh",*/}
+      {/*        position: "relative",*/}
+      {/*      }}*/}
+      {/*    >*/}
+      {/*      <MobileNav className={"mobile-navbar"} />*/}
+      {/*      {props.loading ? null : <Map />}*/}
+      {/*    </Col>*/}
+      {/*  </Row>*/}
+      {/*</Container>*/}
+      {/*<LocationModal />*/}
+      {/*<Route path="/:locationName" render={() => <LocationOpener />} />*/}
     </>
   );
 }
+
 function mapStateToProps(state) {
   return {
     spinning: state.location.loading,
@@ -76,4 +89,4 @@ function mapStateToProps(state) {
   };
 }
 
-export default connect(mapStateToProps)(App);
+export default connect(mapStateToProps, { fetchMetaData })(App);
